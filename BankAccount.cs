15d00ac05d83d980a66db7286efb15c8c6a4d913
Armando Aranda <arandaae19@gmail.com>
2022-05-03 11:22:27 -0500

    First commmit

---
+++ BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        
+        private double balance = 0;
+              
+
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+        
+        public double GetBalance()
+        {
+            return balance;
+            
+        }
+
+       
+        
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userAccount = new BankAccount();
+
+            userAccount.Deposit(100);
+            userAccount.GetBalance();
+            Console.WriteLine(userAccount);
+
+        }
+    }
+}
