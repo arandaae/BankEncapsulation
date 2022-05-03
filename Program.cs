5ad3d26486f634fe49a68da7c24f53feb74a621a
Armando Aranda <arandaae19@gmail.com>
2022-05-03 13:07:28 -0500

    Added comments to Program.cs file

--- Program.cs
+++ Program.cs
@@ -12,28 +12,29 @@ namespace BankEncapsulation
         static void Main(string[] args)
         {
             var userAccount = new BankAccount();
-
+            
+            //Deposit
             Console.WriteLine("Enter your deposit amount");
             var amount = double.Parse(Console.ReadLine());
 
             userAccount.Deposit(amount);
-
             Console.WriteLine();
 
-            Console.WriteLine($"Your Balance is now:");
-            
+            //Balance after deposit
+            Console.WriteLine($"Your Balance is now:");           
             userAccount.GetBalance();
 
             Console.WriteLine();
 
+            //Withdraw
             Console.WriteLine("Would you like to make a withdrawal?");
             var amount2 = double.Parse(Console.ReadLine());
             userAccount.Withdraw(amount2);
 
             Console.WriteLine();
 
+            //Balance after withdrawal
             Console.WriteLine($"Your Balance is now:");
-
             userAccount.GetBalance();
 
 
